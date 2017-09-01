d56257ff88859794154d044dbb95f288aa5f51e0
Jian Wu <jian.wu@samsung.com>
2017-09-01 16:50:45 -0700

    flask server serving angularapp page

--- server/server.py
+++ server/server.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/<path:path>')
+def send_angularapp(path):
+    return send_from_directory("angularapp/dist", path)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
