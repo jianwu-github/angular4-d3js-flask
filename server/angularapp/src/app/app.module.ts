bee77c5bbf4ad0fce3507ca4a56c3135715e51b1
Jian Wu <hellojianwu@gmail.com>
2017-09-03 16:11:49 -0700

    using more Angular Material 2 components with Dashboard UI

--- server/angularapp/src/app/app.module.ts
+++ server/angularapp/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from '@angular/material';
@@ -13,6 +15,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     MaterialModule,
     FlexLayoutModule,
     BrowserAnimationsModule
