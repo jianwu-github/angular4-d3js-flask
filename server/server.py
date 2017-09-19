12a8ffbab6db70925ae8a09c697d22728700db3d
Jian Wu <hellojianwu@gmail.com>
2017-09-19 01:13:56 -0700

    add simple request logging to stdout in Flask Server

--- server/server.py
+++ server/server.py
@@ -1,14 +1,28 @@
+import logging, sys
+from logging import StreamHandler
+from time import strftime
+
 from flask import Flask, request, send_from_directory
+
+stdout_handler = StreamHandler(sys.stdout)
+app_logger = logging.getLogger('__angular4_d3js_flask__')
+app_logger.setLevel(logging.INFO)
+app_logger.addHandler(stdout_handler)
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    ts = strftime('[%Y-%b-%d %H:%M]')
+    app_logger.info('%s default request received, path: /', ts)
+    return 'Flask Server with Angular4 + D3.js v4 is up and running!'
 
 @app.route('/<path:path>')
 def send_angularapp(path):
+    ts = strftime('[%Y-%b-%d %H:%M]')
+    app_logger.info('%s request received, path: /%s', ts, path)
     return send_from_directory("angularapp/dist", path)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app_logger.info("Starting app ...")
+    app.run(host='0.0.0.0', debug = False)
